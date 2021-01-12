f3bc18a64a1e86247c74c043d16091241dd76cfc
CezaryZal <czarek0821@gmail.com>
2021-01-12 23:18:38 +0100

    Append column to allow translate lang to native

--- src/main/resources/template_data_sentence_memo.sql
+++ src/main/resources/template_data_sentence_memo.sql
@@ -6,18 +6,22 @@ DROP TABLE IF EXISTS `sentence`;
 
 CREATE TABLE `sentence`(
                 `id` BIGINT NOT NULL AUTO_INCREMENT,
-                `language_eng` VARCHAR (45) NOT NULL,
-                `language_pol` VARCHAR (45) NOT NULL,
-                `hint` VARCHAR (128) DEFAULT NULL,
-                `replay_level` INTEGER DEFAULT 0,
-                `replay_date` DATE NOT NULL,
+                `language_eng` VARCHAR (32) NOT NULL,
+                `language_pol` VARCHAR (32) NOT NULL,
+                `hint_from_native` VARCHAR (128) DEFAULT NULL,
+                `hint_to_native` VARCHAR (128) DEFAULT NULL,
+                `replay_level_from_native` INTEGER DEFAULT 0,
+                `replay_level_to_native` INTEGER DEFAULT 0,
+                `replay_date_from_native` DATE NOT NULL,
+                `replay_date_to_native` DATE NOT NULL,
+                `date_created` DATE NOT NULL,
                 PRIMARY KEY (`id`)
 ) ENGINE = InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
 
 INSERT INTO `sentence` VALUES
-(1, 'home', 'dom', 'miejsce, nie budynek', 1, '2019-12-15'),
-(2, 'field', 'pole', 'miejsce do okreslania zmiennych', 1, '2019-12-14'),
-(3, 'granted', 'zgoda', 'inaczej pozwolenie(pol)', 1, '2019-12-14'),
-(4, 'suite', 'zestaw', 'pisze się podobnie jak stroj', 1, '2019-12-15');
+(1, 'home', 'dom', 'miejsce, nie budynek', 'znasz', 1, 1, '2019-12-15', '2019-12-15', '2019-12-15'),
+(2, 'field', 'pole', 'miejsce do okreslania zmiennych', 'na roli', 1, 1, '2019-12-14', '2019-12-15', '2019-12-15'),
+(3, 'granted', 'zgoda', 'inaczej pozwolenie(pol)', 'po kłótni', 1, 1, '2019-12-14', '2019-12-15', '2019-12-15'),
+(4, 'suite', 'zestaw', 'pisze się podobnie jak stroj', '... sztućców', 1, 1, '2019-12-15', '2019-12-15', '2019-12-15');
 
 SET FOREIGN_KEY_CHECKS = 1;
