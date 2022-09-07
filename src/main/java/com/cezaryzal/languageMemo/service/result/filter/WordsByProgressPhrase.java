11473fb4a9f042460ec6aa89d93fbcebcc2e0e22
Cezary Zalewski <cezary.zalewski@whirly.pl>
2022-09-07 20:19:33 +0200

    Append WordsByProgressPhrase.class with test units

---
+++ src/main/java/com/cezaryzal/languageMemo/service/result/filter/WordsByProgressPhrase.java
@@ -0,0 +1,38 @@
+package com.cezaryzal.languageMemo.service.result.filter;
+
+import com.cezaryzal.languageMemo.model.CurrentPlayedSentenceComponent;
+
+import java.util.Arrays;
+
+public class WordsByProgressPhrase {
+    private final CurrentPlayedSentenceComponent currentPlayedSentence;
+
+    public WordsByProgressPhrase(CurrentPlayedSentenceComponent currentPlayedSentence) {
+        this.currentPlayedSentence = currentPlayedSentence;
+    }
+
+
+    //TODO validacja czy frazy poniżej 3 liter zostają zamieniane na puste blanki
+    public void catchCorrectWordToProgressPhrase(String[] splittedInputPhrases, String progressPhrase){
+        StringBuilder creatingProgressPhrase = new StringBuilder();
+
+        Arrays.stream(splittedInputPhrases)
+                .forEach(word -> {
+                    if (currentPlayedSentence.getUsedSentence().getCorrectAnswer().contains(word)) {
+                        int index = currentPlayedSentence
+                                .getUsedSentence()
+                                .getCorrectAnswer()
+                                .indexOf(word);
+
+                        if (index != 0)
+                            creatingProgressPhrase
+                                    .append(progressPhrase, 0, index);
+
+                        creatingProgressPhrase
+                                .append(word)
+                                .append(progressPhrase.substring(index + word.length()));
+                    }
+                });
+        currentPlayedSentence.setProgressPhrase(creatingProgressPhrase.toString());
+    }
+}
---
+++ src/test/java/com/cezaryzal/languageMemo/service/result/filter/WordsByProgressPhraseTest.java
@@ -0,0 +1,116 @@
+package com.cezaryzal.languageMemo.service.result.filter;
+
+import com.cezaryzal.languageMemo.model.CurrentPlayedSentenceComponent;
+import com.cezaryzal.languageMemo.repository.entity.Sentence;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WordsByProgressPhraseTest {
+    private Sentence sampleSentenceForTest;
+    private CurrentPlayedSentenceComponent currentPlayedSentenceComponent;
+
+    @Autowired
+    private ReplacementBlankCharacters replacementBlankCharacters;
+
+    @Before
+    public void setup(){
+        sampleSentenceForTest = getSampleSentenceForTest();
+        currentPlayedSentenceComponent = new CurrentPlayedSentenceComponent(replacementBlankCharacters);
+        currentPlayedSentenceComponent.initialProgressPhrase(sampleSentenceForTest);
+    }
+
+    @Test
+    public void catchCorrectWordToProgressPhraseFirstTest() {
+        WordsByProgressPhrase wordsByProgressPhrase = new WordsByProgressPhrase(currentPlayedSentenceComponent);
+        String stubProgressPhrase = "_______________";
+
+        String inputPhrases = "semi";
+        String[] splittedInputPhrases = inputPhrases.split("[ _\\-]");
+        wordsByProgressPhrase.catchCorrectWordToProgressPhrase(splittedInputPhrases, stubProgressPhrase);
+        Assert.assertEquals("semi___________", currentPlayedSentenceComponent.getProgressPhrase());
+        Assert.assertNotEquals("semi-__________", currentPlayedSentenceComponent.getProgressPhrase());
+    }
+
+    @Test
+    public void catchCorrectWordToProgressPhraseSecondTest() {
+        WordsByProgressPhrase wordsByProgressPhrase = new WordsByProgressPhrase(currentPlayedSentenceComponent);
+        String stubProgressPhrase = "_______________";
+        //TODO w testach integracyjnych przepisać przyapdek
+//        String inputSecondPhrases = "se";
+//        String[] splittedInputSecondPhrases = inputSecondPhrases.split("[ _\\-]");
+//        wordsByProgressPhrase.catchCorrectWordToProgressPhrase(splittedInputSecondPhrases, stubProgressPhrase);
+//        Assert.assertEquals("_______________", currentPlayedSentenceComponent.getProgressPhrase());
+//        Assert.assertNotEquals("se___________", currentPlayedSentenceComponent.getProgressPhrase());
+
+        String inputPhrases = "sem";
+        String[] splittedInputPhrases = inputPhrases.split("[ _\\-]");
+        wordsByProgressPhrase.catchCorrectWordToProgressPhrase(splittedInputPhrases, stubProgressPhrase);
+        Assert.assertEquals("sem____________", currentPlayedSentenceComponent.getProgressPhrase());
+        Assert.assertNotEquals("_____________", currentPlayedSentenceComponent.getProgressPhrase());
+    }
+
+    @Test
+    public void catchCorrectWordToProgressPhraseThirdTest() {
+        WordsByProgressPhrase wordsByProgressPhrase = new WordsByProgressPhrase(currentPlayedSentenceComponent);
+        String stubProgressPhrase = "___________________";
+
+        String inputPhrases = "detached";
+        String[] splittedInputPhrases = inputPhrases.split("[ _\\-]");
+        wordsByProgressPhrase.catchCorrectWordToProgressPhrase(splittedInputPhrases, stubProgressPhrase);
+        Assert.assertEquals("_____detached______", currentPlayedSentenceComponent.getProgressPhrase());
+        Assert.assertNotEquals("____-detached______", currentPlayedSentenceComponent.getProgressPhrase());
+    }
+
+    @Test
+    public void catchCorrectWordToProgressPhraseFourthTest() {
+        WordsByProgressPhrase wordsByProgressPhrase = new WordsByProgressPhrase(currentPlayedSentenceComponent);
+        String stubProgressPhrase = "___________________";
+
+        String inputPhrases = "ched";
+        String[] splittedInputPhrases = inputPhrases.split("[ _\\-]");
+        wordsByProgressPhrase.catchCorrectWordToProgressPhrase(splittedInputPhrases, stubProgressPhrase);
+        Assert.assertEquals("_________ched______", currentPlayedSentenceComponent.getProgressPhrase());
+        Assert.assertNotEquals("_____detached______", currentPlayedSentenceComponent.getProgressPhrase());
+    }
+
+    @Test
+    public void catchCorrectWordToProgressPhraseFifthTest() {
+        WordsByProgressPhrase wordsByProgressPhrase = new WordsByProgressPhrase(currentPlayedSentenceComponent);
+        String stubProgressPhrase = "___________________";
+
+        String inputPhrases = "house";
+        String[] splittedInputPhrases = inputPhrases.split("[ _\\-]");
+        wordsByProgressPhrase.catchCorrectWordToProgressPhrase(splittedInputPhrases, stubProgressPhrase);
+        Assert.assertEquals("______________house", currentPlayedSentenceComponent.getProgressPhrase());
+        Assert.assertNotEquals("____________- house", currentPlayedSentenceComponent.getProgressPhrase());
+    }
+
+    @Test
+    public void catchCorrectWordToProgressPhraseSixthTest() {
+        WordsByProgressPhrase wordsByProgressPhrase = new WordsByProgressPhrase(currentPlayedSentenceComponent);
+        String stubProgressPhrase = "___________________";
+
+        String inputPhrases = "ched ";
+        String[] splittedInputPhrases = inputPhrases.split("[ _\\-]");
+        wordsByProgressPhrase.catchCorrectWordToProgressPhrase(splittedInputPhrases, stubProgressPhrase);
+        Assert.assertEquals("_________ched______", currentPlayedSentenceComponent.getProgressPhrase());
+        Assert.assertNotEquals("_________ched _____", currentPlayedSentenceComponent.getProgressPhrase());
+    }
+
+
+    private Sentence getSampleSentenceForTest(){
+        Sentence testingSentence = new Sentence();
+        testingSentence.setId(55L);
+        testingSentence.setClues("bliźniak dom");
+        testingSentence.setCorrectAnswer("semi-detached house");
+        return testingSentence;
+    }
+
+}
