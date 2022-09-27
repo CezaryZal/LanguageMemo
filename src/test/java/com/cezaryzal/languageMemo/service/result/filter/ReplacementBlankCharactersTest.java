56bd0ea3e54f3977a48feb5e0d717fc247c156e6
Cezary Zalewski <cezary.zalewski@whirly.pl>
2022-09-27 22:19:41 +0200

    Refactoring ReplacementBlankCharactersTest.java

--- src/test/java/com/cezaryzal/languageMemo/service/result/filter/ReplacementBlankCharactersTest.java
+++ src/test/java/com/cezaryzal/languageMemo/service/result/filter/ReplacementBlankCharactersTest.java
@@ -2,89 +2,54 @@ package com.cezaryzal.languageMemo.service.result.filter;
 
 import org.junit.Assert;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mock;
-import org.mockito.Mockito;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 public class ReplacementBlankCharactersTest {
-    private final String wordTesting = "flash-cards";
-    private final String specialChars = "*_&-'!?@%^";
-    private final String correctlyReplacementSentenceWithoutSpecialCharsOnEmptyChar = "__________";
-    private final String correctlyReplacementSentenceWithSpecialCharsOnEmptyChar = "_____-____";
+    private final ReplacementBlankCharacters replacementBlankCharacters = new ReplacementBlankCharacters();
 
-    @Mock
-    ReplacementBlankCharacters replacementBlankCharacters;
+    private final String wordTesting = "flash-cards";
+    private final String specialChars = "*#_&-'!?@%^";
+    private final String correctlyReplacementSentenceWithoutSpecialCharsOnEmptyChar = "___________";
+    private final String correctlyReplacementSentenceWithSpecialCharsOnEmptyChar = "_____-_____";
+
 
     @Test
-    public void correctlyReplaceAllSentenceOnEmptyCharTest(){
-        Mockito.when(replacementBlankCharacters
-                .replaceAllSentenceOnEmptyChars(wordTesting))
-                .thenReturn(correctlyReplacementSentenceWithoutSpecialCharsOnEmptyChar);
-
+    public void replaceAllSentenceOnEmptyCharTest(){
         String emptyCharsAfterReplacement =
                 replacementBlankCharacters.replaceAllSentenceOnEmptyChars(wordTesting);
 
         Assert.assertEquals(
                 emptyCharsAfterReplacement,
                 correctlyReplacementSentenceWithoutSpecialCharsOnEmptyChar);
-    }
-
-    @Test
-    public void wronglyReplaceAllSentenceOnEmptyCharTest(){
-        Mockito.when(replacementBlankCharacters
-                .replaceAllSentenceOnEmptyChars(wordTesting))
-                .thenReturn("___");
-
-        String emptyCharsAfterReplacement =
-                replacementBlankCharacters.replaceAllSentenceOnEmptyChars(wordTesting);
 
         Assert.assertNotEquals(
                 emptyCharsAfterReplacement,
-                correctlyReplacementSentenceWithoutSpecialCharsOnEmptyChar);
+                correctlyReplacementSentenceWithSpecialCharsOnEmptyChar);
+        Assert.assertNotEquals(emptyCharsAfterReplacement, wordTesting);
     }
 
     @Test
     public void correctlyReplaceAllSentenceWithSpecialChartsOnEmptyCharTest(){
-        Mockito.when(replacementBlankCharacters
-                .replaceAllSentenceOnEmptyChars(specialChars))
-                .thenReturn(correctlyReplacementSentenceWithoutSpecialCharsOnEmptyChar);
-
         String emptyCharsAfterReplacement =
                 replacementBlankCharacters.replaceAllSentenceOnEmptyChars(specialChars);
 
         Assert.assertEquals(
                 emptyCharsAfterReplacement,
                 correctlyReplacementSentenceWithoutSpecialCharsOnEmptyChar);
+        Assert.assertNotEquals(emptyCharsAfterReplacement, specialChars);
     }
 
     @Test
     public void correctlyReplaceLetterOnEmptyCharTest(){
-        Mockito.when(replacementBlankCharacters
-                .replaceLetterOnEmptyChar(wordTesting))
-                .thenReturn(correctlyReplacementSentenceWithSpecialCharsOnEmptyChar);
-
         String replaceLetterOnEmptyChar =
                 replacementBlankCharacters.replaceLetterOnEmptyChar(wordTesting);
 
         Assert.assertEquals(
                 replaceLetterOnEmptyChar,
                 correctlyReplacementSentenceWithSpecialCharsOnEmptyChar);
-    }
-
-    @Test
-    public void wrongReplaceLetterOnEmptyCharTest(){
-        Mockito.when(replacementBlankCharacters
-                .replaceLetterOnEmptyChar(wordTesting))
-                .thenReturn(correctlyReplacementSentenceWithoutSpecialCharsOnEmptyChar);
-
-        String replaceLetterOnEmptyChar =
-                replacementBlankCharacters.replaceLetterOnEmptyChar(wordTesting);
 
         Assert.assertNotEquals(
                 replaceLetterOnEmptyChar,
-                correctlyReplacementSentenceWithSpecialCharsOnEmptyChar);
+                correctlyReplacementSentenceWithoutSpecialCharsOnEmptyChar);
+        Assert.assertNotEquals(replaceLetterOnEmptyChar, wordTesting);
     }
-
-
 }
